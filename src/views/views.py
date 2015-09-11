db4629200f1209a753b863c78fb1b85c9da5a789
Joakim Möller <joakim.moller@molflow.com>
2015-09-11 12:23:03 +0200

    Rearranging view structures

---
+++ src/views/views.py
@@ -0,0 +1,434 @@
+""" doc
+"""
+from flask import request, send_file
+from flask import render_template, jsonify, abort
+from flask.views import MethodView
+from sqlalchemy import create_engine
+import numpy
+import io
+from matplotlib import use
+use("Agg")
+from matplotlib.pylab import figure
+from date_tools import *
+from level1b_scandata_exporter import *
+from level1b_scanlogdata_exporter import *
+from pg import DB
+from sys import stderr, stdout, stdin, argv, exit
+import matplotlib.pyplot as plt
+from datetime import date, datetime, timedelta
+from matplotlib.ticker import ScalarFormatter, FormatStrFormatter
+from matplotlib import dates, rc
+from dateutil.relativedelta import relativedelta
+import matplotlib
+
+class ListScans(MethodView):
+    """Find all scans"""
+
+    def get(self):
+        """ Get the right info"""
+        start_time = request.args['start_time']
+        end_time = request.args['end_time']
+        date1 = datestring_to_date(start_time)
+        date2 = datestring_to_date(end_time)
+        stw1, stw2 = stw_from_date(date1, date2)
+        engine = create_engine(
+            'postgresql://odinop:***REMOVED***'
+            '@malachite.rss.chalmers.se:5432/odin'
+            )
+        con = engine.connect()
+        query_string = (
+            'select distinct(stw) as scan_id '
+            'from ac_cal_level1b '
+            'where stw between {0} and {1} and freqmode=2;').format(
+                stw1, stw2
+                )
+        result = con.execute(query_string)
+        id_list = list()
+        for row in result:
+            id_list.append(row['scan_id'])
+        return str(stw1) + " " + str(stw2) + " " + str(id_list)
+
+class ViewScan(MethodView):
+    """View of all scans"""
+
+    def get(self, scanno):
+        """get scandata"""
+        engine = create_engine(
+            'postgresql://odinop:***REMOVED***'
+            '@malachite.rss.chalmers.se:5432/odin'
+            )
+        con = engine.connect()
+        query_string = (
+            'select channels, spectra, calstw from ac_level1b '
+            'where calstw={0} order by stw').format(int(scanno))
+        result = con.execute(query_string)
+
+        spectrum = []
+        for row in result:
+            data = numpy.ndarray(
+                shape=(row[0],),
+                dtype='float64',
+                buffer=row[1]
+                )
+            spectrum.append(data.tolist())
+        #data = numpy.vstack((data, data))
+        accept = request.headers['Accept']
+        if "application/json" in accept:
+            return jsonify(test="ok", other=2, data=spectrum)
+        else:
+            return render_template('message.html',text='Don''t know what to do.')
+
+
+class ViewSpectrum(MethodView):
+    """View of all scans"""
+
+    def get(self, scanno):
+        """get spectrum"""
+        engine = create_engine(
+            'postgresql://odinop:***REMOVED***'
+            '@malachite.rss.chalmers.se:5432/odin'
+            )
+        con = engine.connect()
+        query_string = (
+            'select channels, spectra, calstw from ac_level1b '
+            'where stw={0}').format(int(scanno))
+        result = con.execute(query_string)
+        res = result.fetchall()
+        if len(res) == 0:
+            return abort(404)
+        data = numpy.ndarray(
+            shape=(res[0][0],),
+            dtype='float64',
+            buffer=res[0][1]
+            )
+        data = numpy.vstack((data, data))
+        accept = request.headers['Accept']
+        if "application/json" in accept:
+            return jsonify(test="ok", other=2, data=data.tolist())
+        return 'test'
+
+class PlotSpectrum(MethodView):
+    """plots information"""
+    def get(self, scanno):
+        """Serves the plot"""
+        stw = int(scanno)
+        engine = create_engine(
+            'postgresql://odinop:***REMOVED***@malachite.rss.chalmers.se:5432/odin')
+        con = engine.connect()
+        query_string = (
+            'select channels, spectra, backend, skyfreq, lofreq '
+            'from ac_level1b where stw={0}').format(stw)
+        result = con.execute(query_string)
+        res = result.fetchall()
+        backend = res[0][2]
+        skyfreq = res[0][3]
+        lofreq = res[0][4]
+        data = numpy.ndarray(
+            shape=(res[0][0],), 
+            dtype='float64',
+            buffer=res[0][1]
+            )
+        fig = figure()
+        ax = fig.add_axes([.1, .1, .8, .8])
+#        ax.plot(freqs, data, ".")
+        ax.plot(data)
+        buf = io.BytesIO()
+        fig.savefig(buf, format='png')
+        buf.seek(0)
+        return send_file(
+            buf, attachment_filename='plot.png', mimetype='image/png')
+
+class ViewIndex(MethodView):
+    """View of all scans"""
+
+    def get(self):
+        return render_template('index.html')
+
+class ViewScaninfo(MethodView):
+    """plots information"""
+    def get(self, backend,date,):
+        con = db()
+
+        loginfo,date1,date2 = get_scan_logdata(con, backend, date)
+
+        lista = []
+        for ind in range(len(loginfo['ScanID'])):
+            row = []    
+            for item in ['DateTime','FreqMode','StartLat','EndLat','SunZD','AltStart','AltEnd','ScanID']:
+                row.append(loginfo[item][ind])
+            lista.append(row)
+        return render_template('plottest.html', date=date, backend=backend,scanno=loginfo['ScanID'][0],lista=lista)
+
+class ViewScaninfoplot(MethodView):
+    """plots information"""
+    def get(self, backend,date,):
+
+        con = db()
+
+        loginfo,date1,date2 = get_scan_logdata(con, backend, date)
+
+        accept = request.headers['Accept']
+
+        if "application/json" in accept:
+            for item in loginfo.keys():
+                try:
+                    loginfo[item]=loginfo[item].tolist()
+                except:
+                    pass
+            return jsonify(**loginfo)
+        else:
+            fig = plot_loginfo(backend,date1,date2,loginfo)
+            buf = io.BytesIO()
+            fig.savefig(buf, format='png')
+            buf.seek(0)
+            return send_file(
+                buf, attachment_filename='plot.png', mimetype='image/png')
+
+
+class ViewScandata(MethodView):
+    """plots information"""
+    def get(self, backend, scanno):
+        
+        con=db()
+    
+        #export data
+        calstw = int(scanno)
+        spectra = get_scan_data(con, backend, scanno)
+
+        #spectra is a dictionary containing the relevant data
+
+        accept = request.headers['Accept']
+
+        if "application/json" in accept:
+
+            datadict = scan2dictlist(spectra)
+
+            return jsonify(**datadict)
+            
+        else:
+
+            fig=plot_scan(backend,calstw,spectra)
+            con.close()
+            buf = io.BytesIO()
+            fig.savefig(buf, format='png')
+            buf.seek(0)
+
+            return send_file(
+                    buf, attachment_filename='plot.png', mimetype='image/png')
+
+
+class ViewDateInfo(MethodView):
+    """plots information"""
+    def get(self, date):
+        
+        con = db()
+        date1 = datetime.strptime(date, '%Y-%m-%d')
+        date2 = date1 + relativedelta(days = +1)
+        mjd1 = date2mjd(date1)
+        mjd2 = date2mjd(date2)
+        stw1 = mjd2stw(mjd1)
+        stw2 = mjd2stw(mjd2)
+        temp = [stw1, stw2, mjd1, mjd2]
+        query_str = ''' select freqmode, backend, count(distinct(stw)) from
+             ac_cal_level1b
+             join attitude_level1 using(backend,stw)
+             where stw between {0} and {1}
+             and mjd between {2} and {3}
+             group by backend,freqmode
+             order by backend,freqmode '''.format(*temp)
+
+        query = con.query(query_str)
+
+        result = query.dictresult()
+        lista = []
+        datadict = {
+            'Date'     : [],
+            'Backend'  : [],
+            'FreqMode' : [],
+            'NumScan'  : [],
+                    }
+        datadict['Date'].append(str(date1.date()))
+        for row in result:
+            lista.append([date1.date(),row['backend'],row['freqmode'],row['count']])
+            datadict['Backend'].append(row['backend'])
+            datadict['FreqMode'].append(row['freqmode'])
+            datadict['NumScan'].append(row['count']) 
+        con.close()
+        accept = request.headers['Accept']
+
+        if "application/json" in accept:
+            
+
+            return jsonify(**datadict)
+
+        else:
+      
+            return render_template('plottest2.html', scanno=int(1),lista=lista)
+
+
+class ViewDateBackendInfo(MethodView):
+    """plots information"""
+    def get(self, date, backend):
+
+        con = db()
+        date1 = datetime.strptime(date, '%Y-%m-%d')
+        date2 = date1 + relativedelta(days = +1)
+        mjd1 = date2mjd(date1)
+        mjd2 = date2mjd(date2)
+        stw1 = mjd2stw(mjd1)
+        stw2 = mjd2stw(mjd2)
+        temp = [stw1, stw2, mjd1, mjd2, backend]
+        query_str = '''
+             select freqmode, backend, count(distinct(stw)) from
+             ac_cal_level1b
+             join attitude_level1 using(backend,stw)
+             where stw between {0} and {1}
+             and mjd between {2} and {3}
+             and backend='{4}'
+             group by backend,freqmode
+             order by backend,freqmode
+                         '''.format(*temp)
+
+        query = con.query(query_str)
+
+        result = query.dictresult()
+        lista = []
+        for row in result:
+            lista.append([date1.date(),row['backend'],row['freqmode'],row['count']])
+        con.close()
+        accept = request.headers['Accept']
+
+        if "application/json" in accept:
+
+            return jsonify(**lista)
+
+        else:
+
+            return render_template('plottest2.html', scanno=int(1),lista=lista)
+
+
+
+
+class ViewFreqmodeInfo(MethodView):
+    """loginfo for all scans from a given date and freqmode"""
+    def get(self, date, backend, freqmode):
+
+
+        con = db()
+
+        loginfo,date1,date2 = get_scan_logdata(con, backend,date+'T00:00:00',int(freqmode),1)
+
+        lista = []
+        for ind in range(len(loginfo['ScanID'])):
+            row = []
+            row.append( loginfo['DateTime'][ind].date() )
+            for item in ['DateTime','FreqMode','StartLat','EndLat','SunZD','AltStart','AltEnd','ScanID']:
+                row.append(loginfo[item][ind])
+            lista.append(row)
+
+        accept = request.headers['Accept']
+
+        if "application/json" in accept:
+
+            for item in loginfo.keys():
+                try:
+                    loginfo[item]=loginfo[item].tolist()
+                except:
+                    pass
+
+            return jsonify(**loginfo)
+
+        else:
+            return render_template('plottest3.html', date=loginfo['DateTime'][0].date(),
+                                    backend=backend,freqmode=freqmode,lista=lista)
+
+
+class ViewFreqmodeInfoPlot(MethodView):
+    """plots information: loginfo for all scans from a given date and freqmode"""
+    def get(self, date, backend, freqmode):
+
+
+        con = db()
+
+        loginfo,date1,date2 = get_scan_logdata(con, backend,date+'T00:00:00',int(freqmode),1)
+
+        lista = []
+        for ind in range(len(loginfo['ScanID'])):
+            row = []
+            row.append( loginfo['DateTime'][ind].date() )
+            for item in ['DateTime','FreqMode','StartLat','EndLat','SunZD','AltStart','AltEnd','ScanID']:
+                row.append(loginfo[item][ind])
+            lista.append(row)
+
+        accept = request.headers['Accept']
+
+        if "application/json" in accept:
+
+
+            return jsonify(**lista)
+
+        else:
+            fig = plot_loginfo(backend,date1,date2,loginfo)
+            buf = io.BytesIO()
+            fig.savefig(buf, format='png')
+            buf.seek(0)
+            return send_file(
+                    buf, attachment_filename='plot.png', mimetype='image/png')
+
+
+
+class ViewScanSpec(MethodView):
+    """plots information: data from a given scan"""
+    def get(self, date, backend, freqmode, scanno):
+
+        con=db()
+
+        #export data
+        calstw = int(scanno)
+        
+        spectra = get_scan_data(con, backend, freqmode, scanno)
+
+        #spectra is a dictionary containing the relevant data
+
+        accept = request.headers['Accept']
+
+        if "application/json" in accept:
+
+            datadict = scan2dictlist(spectra)
+
+            return jsonify(**datadict)
+
+        else:
+
+            fig=plot_scan(backend,calstw,spectra)
+            con.close()
+            buf = io.BytesIO()
+            fig.savefig(buf, format='png')
+            buf.seek(0)
+
+            return send_file(
+                    buf, attachment_filename='plot.png', mimetype='image/png')
+
+
+
+
+class Test(MethodView):
+    """View of all scans"""
+    def get(self, scanno):
+        a = range(10)
+        return render_template('plottest.html', scanno=int(scanno),lista=a)
+
+
+
+def copyemptydict(a):
+    b = dict()
+    for item in a.keys():
+        b[item] = []
+    return b
+
+
+class db(DB):
+    def __init__(self):
+        #DB.__init__(self,dbname='odin',user='odinop',host='malachite.rss.chalmers.se',passwd='***REMOVED***')
+        DB.__init__(self,dbname='odin',user='odinop',host='postgresql',passwd='***REMOVED***')
+
