b40a794a2c17f767936ab458590e4b12ce050389
Andreas Skyman <andreas.skyman@molflow.com>
2016-01-07 14:01:13 +0100

    Added embryo of time series statistics lookup

--- src/odinapi/views/statistics.py
+++ src/odinapi/views/statistics.py
@@ -34,6 +34,50 @@ class TotalFreqmodeStatistics(MethodView):
         return query_str
 
 
+class FreqmodeTimeseriesStatistics(MethodView):
+    """Statistics of freqmode distribution as time series"""
+    def get(self, version, year=None):
+        """GET"""
+        if version not in ['v1', 'v2', 'v3', 'v4']:
+            abort(404)
+        info_list = []
+        if year is None:
+            for year in xrange(2001, now().year+1):
+                first_date = "{0}-01-01".format(year)
+                last_date = "{0}-12-31".format(year)
+                query_str = self.gen_query(first_date, last_date)
+                result = self.gen_data(query_str)[0]
+                result["year"] = year
+                info_list.append(result)
+        else:
+            for month in xrange(1, 12):
+                first_date = "{0}-{1}-01".format(year, month)
+                last_date = "{0}-{1}-31".format(year, month)
+                query_str = self.gen_query(first_date, last_date)
+                result = self.gen_data(query_str)[0]
+                result["month"] = month
+                info_list.append(result)
+
+        return jsonify(Data=info_list)
+
+    def gen_data(self, query_string):
+        con = DatabaseConnector()
+        query = con.query(query_string)
+        result = query.dictresult()
+        con.close()
+        return result
+
+    def gen_query(self, first_date, last_date):
+        query_str = (
+            "select freqmode, sum(nscans) "
+            "from measurements_cache "
+            "where date between '{0}' and '{1}' "
+            "group by freqmode "
+            "order by freqmode "
+            ).format(first_date, last_date)
+        return query_str
+
+
 class AnnualNscanStatistics(MethodView):
     """Statistics of total number of scans per year"""
     def get(self, version):
@@ -61,7 +105,7 @@ class AnnualNscanStatistics(MethodView):
         query_str = (
             "select sum(nscans) "
             "from measurements_cache "
-            "where date between '{0}' and '{1}'"
+            "where date between '{0}' and '{1}' "
             ).format(first_date, last_date)
         return query_str
 
