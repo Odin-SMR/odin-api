5abcbdcbe7e3f53ecc52ee43ff51ec2f99b82665
Andreas Skyman <andreas.skyman@molflow.com>
2016-02-11 12:55:58 +0100

    Corrected urls for lsp data

--- src/scripts/download_sage.sh
+++ src/scripts/download_sage.sh
@@ -3,27 +3,32 @@ FTP_SERVER="ftp://l5eil01.larc.nasa.gov"
 USER="anonymous"
 PASSWORD="bengt.rydberg@molflow.com"
 
-LOCALSTORAGE="/odin/external/vds-data/Meteor3M_SAGEIII_Level2/v04"
+LOCALSTORAGE="/odin/external/vds-data/Meteor3M_SAGEIII_Level2/"
+LOCALSTORAGE="/home/skymandr/Documents/Work/ODIN/vds/data/Meteor3M_SAGEIII_Level2/"
 
 for year in {2002..2005..1}
 do
-  for month in {00..12..1}
+  for month in {01..12..1}
   do
     for day in {01..31..1}
     do
-      LOCALDIR="${LOCALSTORAGE}/${year}/${month}/"
+      LOCALDIR="${LOCALSTORAGE}/${year}/${month}/v04"
       echo ${LOCALDIR}
       mkdir -p $LOCALDIR
 
-      filepath="/SAGE_III/g3assp.004/${year}.${month}.${day}"
+      filepath="/SAGE_III/g3assp.007/${year}.${month}.${day}"
       file="g3a.ssp.*v04.00"
       URL="${FTP_SERVER}:${filepath}/${file}"
       echo ${URL}
       wget --user=$USER --password=$PASSWORD --backups=0 \
            --directory-prefix=$LOCALDIR $URL
 
-      filepath="/SAGE_III/g3alsp.004/${year}.${month}.${day}"
-      file="g3a.lsp.*v04.00"
+      LOCALDIR="${LOCALSTORAGE}/${year}/${month}/v03"
+      echo ${LOCALDIR}
+      mkdir -p $LOCALDIR
+
+      filepath="/SAGE_III/g3alsp.003/${year}.${month}.${day}"
+      file="g3a.lsp.*v03.00"
       URL="${FTP_SERVER}:${filepath}/${file}"
       echo ${URL}
       wget --user=$USER --password=$PASSWORD --backups=0 \
