1c2bd1eff14b04813ae0102c2358d8a3f42e5c2f
Andreas Skyman <andreas.skyman@molflow.com>
2017-04-04 15:09:05 +0200

    Changed to xvfb-run in run_all_tests.sh
    Summary:
    Changed to xvfb-run in run_all_tests.sh since it is a more
    stable way of doing it, and the way we do it in EoNavAPI
    
    Test Plan: Jenkins
    
    Reviewers: joakim, gregoire
    
    Reviewed By: gregoire
    
    Subscribers: jenkins
    
    Differential Revision: https://phabricator.molflow.com/D314

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -8,8 +8,5 @@ virtualenv env
 export VIRTUAL_ENV="${PWD}/env"
 export PATH="${PWD}/env/bin:${PATH}"
 export PATH="/usr/lib/chromium-browser:${PATH}"
-Xvfb :42 -ac &
-export DISPLAY=:42
 pip install -r test-requirements.txt
-py.test --runslow --runsystem --junitxml=result.xml
-killall Xvfb
+xvfb-run py.test --runslow --runsystem --junitxml=result.xml $*
