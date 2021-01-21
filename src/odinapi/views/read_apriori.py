4ef23097b6fa6f22faa76cdd8db02a2a0806c475
BengtRydberg <48437915+BengtRydberg@users.noreply.github.com>
2021-01-21 08:27:17 +0100

    interpolating for apriori data (#100)
    * corrected interpolation for apriori data
    
    * handling of doy interpolation around the start/end of the year
    
    * merge two tests
    
    * simplified
    
    * refactoring and adding some tests.
    
    * Update src/test/views/test_read_apriori.py
    
    Co-authored-by: Andreas Skyman <andreas.skyman@molflow.com>
    
    Co-authored-by: Andreas Skyman <andreas.skyman@molflow.com>

--- src/odinapi/views/read_apriori.py
+++ src/odinapi/views/read_apriori.py
@@ -2,6 +2,10 @@ import os
 
 import numpy as np
 import scipy.io as sio
+from typing import Tuple
+
+
+DAYS_PER_YEAR = 365  # neglect leap year
 
 
 def get_datadict(data, source):
@@ -30,36 +34,51 @@ def get_datadict(data, source):
     }
 
 
+def get_interpolation_weights(
+    xs: np.array, x: float
+) -> Tuple[int, int, float, float]:
+    assert x > xs[0] and x <= xs[-1]
+    ind2 = np.argmax(x <= xs)
+    ind1 = ind2 - 1
+    dx = xs[ind2] - xs[ind1]
+    w1 = (xs[ind2] - x) / dx
+    return ind1, ind2, w1, 1. - w1
+
+
+def get_interpolation_weights_for_lat(
+    lats: np.array, lat: float
+) -> Tuple[int, int, float, float]:
+    if lat <= lats[0]:
+        return 0, lats.size - 1, 1., 0.
+    if lat >= lats[-1]:
+        return 0, lats.size - 1, 0., 1.
+    return get_interpolation_weights(lats, lat)
+
+
+def get_interpolation_weights_for_doy(
+    xs: np.array, x: float
+) -> Tuple[int, int, float, float]:
+    if (x <= xs[0] or x >= xs[-1]):
+        dx = DAYS_PER_YEAR + xs[0] - xs[-1]
+        xi = min(x, DAYS_PER_YEAR)  # neglect leap year
+        w1 = ((xi - xs[-1]) % DAYS_PER_YEAR) / dx
+        return 0, xs.size - 1, w1, 1. - w1
+    return get_interpolation_weights(xs, x)
+
+
 def get_vmr_interpolated_for_doy(vmr, doys, doy):
-    deltas = np.min(
-        [
-            (doys[:, 0].astype(float) - doy) % 365,
-            (doy - doys[:, 0].astype(float)) % 365,
-        ],
-        axis=0,
+    ind1, ind2, w1, w2 = get_interpolation_weights_for_doy(
+        doys.flatten(), doy
     )
-    ind1, ind2 = np.arange(deltas.size)[np.argsort(deltas)][:2]
-    doy1 = float(doys[ind1].item())
-    doy2 = float(doys[ind2].item())
-    ddoy = min((doy1 - doy2) % 365, (doy2 - doy1) % 365)
-    w1 = deltas[ind2] / ddoy
-    w2 = deltas[ind1] / ddoy
     vmr = vmr[:, :, :, ind1] * w1 + vmr[:, :, :, ind2] * w2
     return vmr[:, :, 0]
 
 
 def get_vmr_interpolated_for_lat(vmr, latitudes, latitude):
-    deltas = np.abs(latitudes[:, 0] - latitude)
-    ind1, ind2 = np.arange(deltas.size)[np.argsort(deltas)][:2]
-    lat1 = latitudes[ind1].item()
-    lat2 = latitudes[ind2].item()
-    if np.sign(lat1 - latitude) != np.sign(lat2 - latitude):
-        dlat = np.abs(lat1 - lat2)
-        w1 = deltas[ind2] / dlat
-        w2 = deltas[ind1] / dlat
-        return vmr[:, ind1] * w1 + vmr[:, ind2] * w2
-
-    return vmr[:, ind1]
+    ind1, ind2, w1, w2 = get_interpolation_weights_for_lat(
+        latitudes.flatten(), latitude
+    )
+    return vmr[:, ind1] * w1 + vmr[:, ind2] * w2
 
 
 def get_apriori(
@@ -79,10 +98,6 @@ def get_apriori(
     doy = float(day_of_year)
     vmr = get_vmr_interpolated_for_doy(datadict['vmr'], datadict['doy'], doy)
 
-    # a priori data is gridded on a latitude grid
-    # covering -85 to 85:
-    # below we make sure latitude is within these limits
-    latitude = max(min(float(latitude), 85.0), -85.0)
     vmr = get_vmr_interpolated_for_lat(vmr, datadict['latitude'], latitude)
 
     return {
--- src/test/views/test_read_apriori.py
+++ src/test/views/test_read_apriori.py
@@ -36,6 +36,89 @@ def test_returns_altitude_for_mipas(co_mipas):
     assert altitudes == list(range(18000, 50000, 1000))
 
 
+@pytest.mark.parametrize("x,expect", (
+    (-79, (0, 1, 0.95, 0.05)),
+    (-70, (0, 1, 0.5, 0.5)),
+    (-68, (0, 1, 0.4, 0.6)),
+    (-60, (0, 1, 0.0, 1.0)),
+    (-50, (1, 2, 0.5, 0.5)),
+    (60, (3, 4, 0.0, 1.0)),
+    (79, (4, 5, 0.05, 0.95)),
+))
+def test_get_interpolation_weights(x, expect):
+    xs = np.array([-80, -60, -40, 40, 60, 80])
+    id1, id2, w1, w2 = read_apriori.get_interpolation_weights(
+        xs, x)
+    np.testing.assert_allclose((id1, id2, w1, w2), expect, atol=1e-6)
+
+
+@pytest.mark.parametrize("doy,expect", (
+    (0, (0, 5, 0.5, 0.5)),  # equal weights to id1 and id2
+    (3, (0, 5, 0.8, 0.2)),
+    (5, (0, 5, 1.0, 0.0)),
+    (360, (0, 5, 0.0, 1.0)),
+    (362, (0, 5, 0.2, 0.8)),
+    (365, (0, 5, 0.5, 0.5)),  # equal weights to id1 and id2
+    (366, (0, 5, 0.5, 0.5)),  # doy 366 gives same result as doy 365
+))
+def test_get_interpolation_weights_for_doy(doy, expect):
+    doys = np.array([5, 10, 15, 350, 355, 360])
+    id1, id2, w1, w2 = read_apriori.get_interpolation_weights_for_doy(
+        doys, doy)
+    np.testing.assert_allclose((id1, id2, w1, w2), expect, atol=1e-6)
+
+
+@pytest.mark.parametrize("lat,expect", (
+    (-90, (0, 5, 1.0, 0.0)),  # all weight to id1
+    (-80, (0, 5, 1.0, 0.0)),
+    (-70, (0, 1, 0.5, 0.5)),
+    (-68, (0, 1, 0.4, 0.6)),
+    (-60, (0, 1, 0.0, 1.0)),
+    (-50, (1, 2, 0.5, 0.5)),
+    (60, (3, 4, 0.0, 1.0)),
+    (80, (0, 5, 0.0, 1.0)),
+    (90, (0, 5, 0.0, 1.0)),  # all weight to id
+))
+def test_get_interpolation_weights_for_lat(lat, expect):
+    lats = np.array([-80, -60, -40, 40, 60, 80])
+    id1, id2, w1, w2 = read_apriori.get_interpolation_weights_for_lat(
+        lats, lat)
+    np.testing.assert_allclose((id1, id2, w1, w2), expect, atol=1e-6)
+
+
+@pytest.mark.parametrize("lat,expect", (
+    (-85, 1),
+    (-80, 1),
+    (-40, 1.5),
+    (80, 3),
+))
+def test_get_vmr_interpolated_for_lat(lat, expect):
+    vmrs = np.zeros((1, 3))
+    vmrs[0, 0] = 1.
+    vmrs[0, 1] = 2.
+    vmrs[0, 2] = 3.
+    lats = np.array([-80, 0, 80])
+    vmr = read_apriori.get_vmr_interpolated_for_lat(
+        vmrs, lats, lat)
+    assert vmr == expect
+
+
+@pytest.mark.parametrize("doy,expect", (
+    (1, 2.5),
+    (15, 1),
+    (30, 1.5),
+))
+def test_get_vmr_interpolated_for_doy(doy, expect):
+    vmrs = np.zeros((1, 1, 1, 3))
+    vmrs[0, 0, 0, 0] = 1.
+    vmrs[0, 0, 0, 1] = 2.
+    vmrs[0, 0, 0, 2] = 4.
+    doys = np.array([15, 45, 352])
+    vmr = read_apriori.get_vmr_interpolated_for_doy(
+        vmrs, doys, doy)
+    assert vmr.item() == expect
+
+
 def test_returns_expected_data_keys():
     species = 'CO2'
     day_of_year = 14
@@ -77,6 +160,34 @@ def test_returns_expected_pressure():
     ])
 
 
+@pytest.mark.parametrize("species,doy,latitude,source,expect", (
+    ('CO2', 1., -90., None, 0.0003626),
+    ('CO2', 1., 35., None, 0.0003626),
+    ('CO2', 15., 35., None, 0.0003626),
+    ('CO2', 180., 35., None, 0.0003626),
+    ('CO2', 350., 35., None, 0.0003626),
+    ('CO2', 365., 35., None, 0.0003626),
+    ('CO2', 365., 80., None, 0.0003626),
+    ('CO2', 365., 90., None, 0.0003626),
+    ('CO', 1., 80., 'mipas', 0.34654013),
+    ('CO', 10., 80., 'mipas', 0.3749516),
+    ('CO', 30., 80., 'mipas', 0.2280777),
+    ('CO', 335., 80., 'mipas', 0.1782076),
+    ('CO', 355., 80., 'mipas', 0.3118149),
+    ('CO', 365., 80., 'mipas', 0.3433833),
+))
+def test_get_apriori_co2_vmr_does_not_vary(
+    species, doy, latitude, source, expect
+):
+    # CO2 is constant for a given altitude in the used apriori data.
+    # Check that returned data is constant for CO2, but not constant
+    # for all species.
+    data = read_apriori.get_apriori(
+        species, doy, latitude, source=source, datadir=DATADIR,
+    )
+    assert data['vmr'][20] == pytest.approx(expect, abs=1e-7)
+
+
 def test_returns_expected_vmr():
     species = 'CO2'
     day_of_year = 14
@@ -89,20 +200,6 @@ def test_returns_expected_vmr():
         [0.000365] * 16 + [0.000364, 0.000364, 0.000363, 0.000363],
         atol=0.0000005,
     )
-
-
-@pytest.mark.parametrize('lat,expect', (
-    (90, 85),
-    (-90, -85),
-    (3, 3),
-))
-def test_clip_lats_outside_range(lat, expect):
-    species = 'CO2'
-    day_of_year = 14
-    data = read_apriori.get_apriori(
-        species, day_of_year, lat, datadir=DATADIR,
-    )
-    assert data['latitude'] == expect
 
 
 def test_using_alternative_source():
