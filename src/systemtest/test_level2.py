f53d306667f8f597b45321f27a003fc5ed71685d
bengt.rydberg <bengt.rydberg@molflow.com>
2021-04-28 18:29:55 +0200

    fixing some failing tests

--- src/systemtest/test_level2.py
+++ src/systemtest/test_level2.py
@@ -422,7 +422,7 @@ class TestReadLevel2:
                 ])
 
             assert scan['ScanID'] == urlinfo.scan_id
-            assert scan['Date'] == '2015-01-12T00:25:03.220790'
+            assert scan['Date'] == '2015-01-12T00:22:33.349085'
 
     @pytest.mark.parametrize('version,comment,expected_scans', (
         (
