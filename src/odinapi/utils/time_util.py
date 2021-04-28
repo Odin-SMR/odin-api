16fae4a66a5f715865a217c5eb1d191a87c8a570
bengt.rydberg <bengt.rydberg@molflow.com>
2021-04-28 17:23:25 +0200

    updating time_util

--- src/odinapi/utils/time_util.py
+++ src/odinapi/utils/time_util.py
@@ -1,15 +1,19 @@
 from datetime import datetime, timedelta
 
-MJD0 = 56416.7782534
-stw0 = 6161431982
-rate = 1/16.0016444
+
+MJD_ODIN_REBOOT = 59247
 MJD_START_DATE = datetime(1858, 11, 17)
 
 
 def datetime2mjd(dt):
     diff = dt - MJD_START_DATE
-    return diff.days + (
-        diff.seconds + diff.microseconds*1e-6)*datetime2mjd.days_per_second
+    return (
+        diff.days
+        + (
+              diff.seconds +
+              diff.microseconds * 1e-6
+        ) * datetime2mjd.days_per_second
+    )
 
 
 datetime2mjd.days_per_second = 1. / 60 / 60 / 24
@@ -19,14 +23,43 @@ def datetime2stw(dt):
     return mjd2stw(datetime2mjd(dt))
 
 
-def mjd2stw(mjd1):
-    stw = (mjd1 - MJD0) * 86400.0 / rate + stw0
-    return int(stw)
+def mjd2stw(mjd):
+    """
+    Convert from mjd (modified julian date) to stw (satellite time word
+    or the internal time of Odin). The conversion is not exact so the
+    function should only be used when an exakt stw is not needed. It is
+    a close to linear relationship between mjd and stw, except that a big
+    jump is found around the Odin reboot date in February 2021.
+    """
+    if mjd >= MJD_ODIN_REBOOT:
+        a = -6.89789762 * 1e10
+        b = 1.38121393 * 1e6
+        c = 1.12346880 * 1e-2
+        d = 0.
+    else:
+        a = - 7.18192098 * 1e10
+        b = 1.38161488 * 1e6
+        c = 1.59079812 * 1e-2
+        d = - 9.05026091 * 1e-8
+    return int(a + b * mjd + c * mjd ** 2 + d * mjd ** 3)
 
 
-def stw2mjd(stw1):
-    mjd = (stw1 - stw0) * rate / 86400.0 + MJD0
-    return mjd
+def stw2mjd(stw):
+    """
+    Convert from stw to mjd in an approximate manner.
+    """
+    if stw >= mjd2stw(MJD_ODIN_REBOOT):
+        # if stw later than odin reboot
+        a = 4.99205631 * 1e4
+        b = 7.23413455 * 1e-7
+        c = -4.25743315 * 1e-21
+        d = 0.
+    else:
+        a = 5.19601792 * 1e4
+        b = 7.23308989 * 1e-7
+        c = -6.81289563 * 1e-22
+        d = 2.47714080 * 1e-32
+    return (a + b * stw + c * stw ** 2 + d * stw ** 3)
 
 
 def mjd2datetime(mjd):
