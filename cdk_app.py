8549eabfc85de4fe75b186befeeca186bdbf515d
Joakim Möller <joakim.moller@molflow.com>
2023-05-26 11:31:31 +0200

    adjust

--- cdk_app.py
+++ cdk_app.py
@@ -1,6 +1,6 @@
 from aws_cdk import App, Environment
 
-from odin_api_stack.odin_api_stack import OdinAPIStack
+from odin_api_stack.odin_api import OdinAPIStack
 from odin_api_stack.config import ODIN_AWS_ACCOUNT, ODIN_AWS_REGION
 
 
@@ -8,4 +8,3 @@ env = Environment(account=ODIN_AWS_ACCOUNT, region=ODIN_AWS_REGION)
 app = App()
 OdinAPIStack(app, "OdinAPIStack", env=env)
 app.synth()
-
