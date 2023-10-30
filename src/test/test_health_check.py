2b84f2bcac507590ead45b6c59f27a1cd793a149
Joakim Möller <joakim.moller@molflow.com>
2023-10-30 13:02:06 +0100

    Health (#177)
    * Simple health_check endpoint
    
    AWS application load balancers and Fargate services wants
    to know if the api is up.
    
    * black had options

---
+++ src/test/test_health_check.py
@@ -0,0 +1,11 @@
+from odinapi.api import create_app
+
+from odinapi.odin_config import TestConfig
+
+
+def test_health_check():
+    config = TestConfig()
+    app = create_app(config)
+    client = app.test_client()
+    r = client.get("/rest_api/health_check")
+    assert r.status_code == 200
