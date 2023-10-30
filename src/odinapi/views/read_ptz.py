ab316d38b8af1812cf9a4fe0dee3e5dfb7cb37c6
Joakim Möller <joakim.moller@molflow.com>
2023-10-30 10:12:03 +0100

    Odin api reads PTZ from parquet source (#175)
    * Odin api reads PTZ from  parquet source
    
    remove donaletty
    read parquet with pyarrow
    
    * Reduce logging for some very verbose loggers (#176)
    
    * Reduce logging for some very vebose loggers
    
    * Odin api reads PTZ from  parquet source
    
    remove donaletty
    read parquet with pyarrow
    
    * Threads and boto3 are not friends
    
    gunicorn config
    more debug logs on s3 ops
    less logging from verbose loggers
    
    * Update logconf.yaml
    
    ---------
    
    Co-authored-by: Andreas Skyman <andreas.skyman@molflow.com>

---
+++ src/odinapi/views/read_ptz.py
@@ -0,0 +1,75 @@
+import logging
+from typing import List, Literal, TypedDict
+
+import pyarrow.compute as pc  # type: ignore
+import pyarrow.dataset as ds  # type: ignore
+import s3fs  # type: ignore
+
+logger = logging.getLogger("odin.ptz")
+
+Backend = Literal["AC1", "AC2"]
+
+
+class PTZ(TypedDict):
+    Pressure: list[float]
+    Temperature: list[float]
+    Altitude: list[float]
+    Latitude: float
+    Longitude: float
+    MJD: float
+
+
+def prefix_names(stw: int) -> List[str]:
+    dir_name = stw >> 6 * 4
+    return [f"{dir_name:x}", f"{dir_name-1:x}"]
+
+
+def get_ptz(
+    backend: Backend, scanid: int, mjd: int, lat: float, lon: float
+) -> PTZ | None:
+    s3 = s3fs.S3FileSystem()
+    result: PTZ | None = None
+    for prefix in prefix_names(scanid):
+        s3_path = f"odin-zpt/{backend.lower()}/{prefix}"
+        try:
+            dataset = ds.dataset(s3_path, format="parquet", filesystem=s3)
+        except FileNotFoundError:
+            logger.warning(
+                "Path does not exist: %s, looking for scanid %x(%i)",
+                s3_path,
+                scanid,
+                scanid,
+            )
+            continue
+        except Exception as e:
+            logger.error("Error reading dataset in %s: %s", s3_path, e)
+            continue
+        try:
+            table = dataset.to_table(filter=ds.field("ScanID") == scanid)
+        except Exception as e:
+            logger.error(
+                "Error exporting dataset to table with ScanID filter (%i) from %s: %s",
+                scanid,
+                s3_path,
+                e,
+            )
+            continue
+        if table.num_rows == 0:
+            logger.debug(
+                "No ptz data found for scanid %x(%i) in path %s",
+                scanid,
+                scanid,
+                s3_path,
+            )
+            continue
+        ptz = table.sort_by("z")
+        logger.debug("found scanid %x(%i) in path %s", scanid, scanid, s3_path)
+        result = PTZ(
+            Altitude=pc.multiply(ptz["z"], 1000).to_pylist(),  # type:ignore
+            Pressure=pc.round(pc.multiply(ptz["p"], 100), ndigits=8).to_pylist(),  # type: ignore
+            Temperature=pc.round(ptz["t"], ndigits=3).to_pylist(),  # type:ignore
+            Latitude=lat,
+            Longitude=lon,
+            MJD=mjd,
+        )
+    return result
